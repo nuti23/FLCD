69aaca569a3669298c6c44a98cb2ad58b105df75
Maria Nutescu <72079445+nuti23@users.noreply.github.com>
2022-11-16 13:04:41 +0200

    Update menu.py

--- lab4/menu.py
+++ lab4/menu.py
@@ -21,6 +21,13 @@ class Menu:
     def get_transitions(self):
         print(self.finite_automata.delta)
 
+    def is_dfa(self):
+        print(self.finite_automata.is_dfa())
+
+    def is_accepted(self):
+        sequence = input('Read sequence:')
+        print(self.finite_automata.is_accepted(sequence))
+
     def menu(self):
         print("0) Exit")
         print("1) States")
@@ -28,11 +35,14 @@ class Menu:
         print("3) Initial state")
         print("4) Final states")
         print("5) Transitions")
+        print("6) Is DFA?")
+        print("7) Is accepted?")
 
 
     def run(self):
         commands = {'1': self.get_states, '2': self.get_alphabet, '3': self.get_initial_state,
-                    '4': self.get_final_states, '5': self.get_transitions}
+                    '4': self.get_final_states, '5': self.get_transitions, '6': self.is_dfa,
+                    '7': self.is_accepted}
         exit = False
         while not exit:
             self.menu()
