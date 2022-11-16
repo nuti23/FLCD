544da94a2a726593386d4bef8fcb90aa1a61c6dd
Maria Nutescu <72079445+nuti23@users.noreply.github.com>
2022-11-16 13:03:21 +0200

    Update finite_automata.py

--- lab4/finite_automata.py
+++ lab4/finite_automata.py
@@ -1,8 +1,35 @@
 
 class FiniteAutomata:
     def __init__(self, Q, A, q0, F, S):
+        # states
         self.Q = Q
+        # alphabet
         self.A = A
+        # initial state
         self.q0 = q0
+        # final state
         self.F = F
+        # transitions
         self.delta = S
+
+    def is_dfa(self):
+        print("keys: ")
+        for elem in self.delta.keys():
+            print(elem, self.delta[elem])
+            if len(self.delta[elem]) > 1:
+                return False
+        return True
+
+    def is_accepted(self, sequence):
+        if self.is_dfa():
+            current = self.q0
+            print("steps: ")
+            for symbol in sequence:
+                if (current, symbol) in self.delta.keys():
+                    current = self.delta[(current, symbol)][0]
+                    print(current, symbol)
+                else:
+                    return False
+            return current in self.F
+        return False
+
